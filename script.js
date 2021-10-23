9563f2febb463a0406a982e927fba45742aeff96
Yuri <ypdmarch@gmail.com>
2021-10-23 17:55:32 +0200

    Github API connection and Linkedin added

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const linksSocialMedia = {
 	github: "YuriMarch",
-	youtube: "",
+	// youtube: "",
 	facebook: "yuri.march.7",
 	instagram: "yuri.march",
-	twitter: "",
+	// twitter: "",
+	linkedin: "in/yuri-march",
 };
 
 function changeSocialMedia() {
@@ -13,4 +14,19 @@ function changeSocialMedia() {
 	}
 }
 
+function getGithubProfileInfos() {
+	const url = `https://api.github.com/users/${linksSocialMedia.github}`;
+
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			userName.textContent = data.name;
+			userBio.textContent = data.bio;
+			userLink.href = data.html_url;
+			userLogin.textContent = data.login;
+			profileImage.src = data.avatar_url;
+		});
+}
+
 changeSocialMedia();
+getGithubProfileInfos();
